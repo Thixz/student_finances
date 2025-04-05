1bd18bb9979df6c0363968af5206a1fa5f196f34
Thiago Costa <thiago.costa@gtrconsultoria.com.br>
2025-04-05 15:31:54 -0300

    new: Aplicação adicionada ao docker

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
+import "dotenv/config";
 import { app } from "./app";
 
-app.listen({ host: "0.0.0.0", port: 3333 }).then(() => {
-  console.log("Application listening on port 3333");
+app.listen({ host: "0.0.0.0", port: Number(process.env.PORT) || 3333 }).then(() => {
+  console.log(`🚀 API is running at http://localhost:${process.env.PORT}/docs`);
 });
